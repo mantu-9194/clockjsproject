d77f02da9232dc8b174ab244d25a70af7aaf4e29
Mahantesh kumar <97968290+mantu-9194@users.noreply.github.com>
2022-03-31 17:19:12 +0530

    Update cl.js

--- cl.js
+++ cl.js
@@ -8,7 +8,7 @@ function clock(){
     var  hrs=time.getHours();
     var  mins=time.getMinutes();
     var  secs=time.getSeconds();
-    var  am_pm="PM";
+    var  am_pm="AM";
 
 
 
@@ -20,7 +20,7 @@ function clock(){
     }
     if(hrs>12){
         hrs=hrs-12;
-        am_pm="AM"
+        am_pm="PM"
     }
     if(hrs<10){
         hrs="0"+hrs;
